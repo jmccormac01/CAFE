96689beebde0739405a3d7adb1213b7251d250f8
James McCormac <jmccormac001@gmail.com>
2016-10-04 03:02:58 +0100

    finished ingestCafeResults.py v1.0

--- ingestCafeResults.py
+++ ingestCafeResults.py
@@ -23,73 +23,97 @@ Results file has the following structure:
     16- Signal to noise ratio at ~5150A
     17- path to the CCF plot file
 """
+import os
 import sys
+import argparse as ap
 import pymysql
 
+# pylint: disable = invalid-name
+
+def argParse():
+    """
+    Parse the command line arguments
+    """
+    parser = ap.ArgumentParser()
+    parser.add_argument('--ingest',
+                        help='Ingest the results to the database',
+                        action='store_true')
+    return parser.parse_args()
+
+RESULTS_FILE = 'results.txt'
+
 if __name__ == '__main__':
+    args = argParse()
     db = pymysql.connect(host='localhost',
                          db='eblm',
                          password='mysqlpassword')
-    f = open('results.txt').readlines()
-    for line in f:
-        ls = line.rstrip().split()
-        if len(ls) != 18:
-            print('ERROR: Wrong number of columns in results.txt')
-            sys.exit(1)
-        obj = ls[0]
-        if obj.startswith('1SWASP'):
-            swasp_id = obj
-        else:
-            swasp_id = None
-        bjd_mid = ls[1]
-        mask_rv = ls[2]
-        mask_rv_err = ls[3]
-        bisector = ls[4]
-        bisector_err = ls[5]
-        mask_ccf_height = ls[13]
-        mask_ccf_fwhm = ls[14]
-        snr_5150 = ls[16]
-        pdf_name = ls[17].split('/')[-1]
-        image_id = '{}.fits'.format(pdf_name.split('.')[0])
-        mask = pdf_name.split('.')[1].split('_')[2]
-        qry = """
-            INSERT INTO eblm_cafe (
-                image_id,
-                swasp_id,
-                object_name,
-                bjd_mid,
-                mask,
-                mask_rv,
-                mask_rv_err,
-                mask_ccf_height,
-                mask_ccf_fwhm,
-                bisector,
-                bisector_err,
-                snr5150
-                )
-            VALUES (
-                '{}',
-                '{}',
-                '{}',
-                {},
-                '{}',
-                {},
-                {},
-                {},
-                {},
-                {},
-                {},
-                {}
-            """.format(image_id,
-                       swasp_id,
-                       obj,
-                       bjd_mid,
-                       mask,
-                       mask_rv,
-                       mask_rv_err,
-                       mask_ccf_height,
-                       mask_ccf_fwhm,
-                       bisector,
-                       bisector_err,
-                       snr_5150)
-        print(qry)
+    if os.path.exists(RESULTS_FILE):
+        f = open(RESULTS_FILE).readlines()
+        for line in f:
+            ls = line.rstrip().split()
+            if len(ls) != 18:
+                print('ERROR: Wrong number of columns in results.txt')
+                sys.exit(1)
+            obj = ls[0]
+            if obj.startswith('1SWASP'):
+                swasp_id = obj
+            else:
+                swasp_id = None
+            bjd_mid = ls[1]
+            mask_rv = ls[2]
+            mask_rv_err = ls[3]
+            bisector = ls[4]
+            bisector_err = ls[5]
+            mask_ccf_height = ls[13]
+            mask_ccf_fwhm = ls[14]
+            snr_5150 = ls[16]
+            pdf_name = ls[17].split('/')[-1]
+            image_id = '{}.fits'.format(pdf_name.split('.')[0])
+            mask = pdf_name.split('.')[-2].split('_')[-1]
+            qry = """
+                INSERT INTO eblm_cafe (
+                    image_id,
+                    swasp_id,
+                    object_name,
+                    bjd_mid,
+                    mask,
+                    mask_rv,
+                    mask_rv_err,
+                    mask_ccf_height,
+                    mask_ccf_fwhm,
+                    bisector,
+                    bisector_err,
+                    snr_5150
+                    )
+                VALUES (
+                    '{}',
+                    '{}',
+                    '{}',
+                    {},
+                    '{}',
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {},
+                    {})
+                """.format(image_id,
+                           swasp_id,
+                           obj,
+                           bjd_mid,
+                           mask,
+                           mask_rv,
+                           mask_rv_err,
+                           mask_ccf_height,
+                           mask_ccf_fwhm,
+                           bisector,
+                           bisector_err,
+                           snr_5150)
+            print(qry)
+            if args.ingest:
+                with db.cursor() as cur:
+                    cur.execute(qry)
+                    db.commit()
+    else:
+        print('{} not found...'.format(RESULTS_FILE))
